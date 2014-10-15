b3457718b563c6c20f6aa32d9a325fd86ffaae25
kfehlberg <fehlbergkatie@gmail.com>
2014-10-15 17:28:06 -0500

    Update js to target banner and balloon obj
    change from targeting svg elements within objects.  now, transforms can
    be seen in browsers without SMIL

--- js/bday_animate.js
+++ js/bday_animate.js
@@ -16,31 +16,30 @@ $( window ).load(function() {
   //  Balloons SVG Variables
   //================================================
   var balloonsObj = document.getElementById("balloons_obj");  // Get the Object by ID
-  var svgBalloonsDoc = balloonsObj.contentDocument;           // Get the SVG document inside the Object tag
+  // var svgBalloonsDoc = balloonsObj.contentDocument;           // Get the SVG document inside the Object tag
   
-  var bRow = svgBalloonsDoc.getElementById("b_row");
+  // var bRow = svgBalloonsDoc.getElementById("b_row");
 
-  var bMinDistance = (function() {
-    var balloons = svgBalloonsDoc.getElementById("balloons_svg");
-    var bSvgHeight = $(balloons).height(); 
-    // console.log("balloonSVG height = " + bSvgHeight);
-    var bSvgWidth = $(balloons).width();
-    // console.log("balloonSVG width = " + bSvgWidth);
-    if (bSvgHeight > bSvgWidth) {
-      return ( bSvgHeight / bSvgWidth ) * ( -500 );
-    } else {
-      return ( -500 );
-    }
-  }() );
+  // var bMinDistance = (function() {
+  //   var balloons = svgBalloonsDoc.getElementById("balloons_svg");
+  //   var bSvgHeight = $(balloons).height(); 
+  //   // console.log("balloonSVG height = " + bSvgHeight);
+  //   var bSvgWidth = $(balloons).width();
+  //   // console.log("balloonSVG width = " + bSvgWidth);
+  //   if (bSvgHeight > bSvgWidth) {
+  //     return ( bSvgHeight / bSvgWidth ) * ( -500 );
+  //   } else {
+  //     return ( -500 );
+  //   }
+  // }() );
 
-  // console.log("min vertical distance for balloons to float = " + bMinDistance);
   //================================================
   //  Banner SVG Variables
   //================================================ 
   var bannerObj = document.getElementById("banner_obj");      // Get the Object by ID
-  var svgBannerDoc = bannerObj.contentDocument;               // Get the SVG document inside the Object tag
+  // var svgBannerDoc = bannerObj.contentDocument;               // Get the SVG document inside the Object tag
   
-  var banner = svgBannerDoc.getElementById("banner");
+  // var banner = svgBannerDoc.getElementById("banner");
   //================================================
   //  DOM Variables
   //    - when using velocity.js sequence, define variables using DOM 
@@ -49,6 +48,21 @@ $( window ).load(function() {
   var underlay = document.getElementById("underlay");
   var blowOutButton = document.getElementById("blow_out");
   var replayButton = document.getElementById("replay");
+
+  var minYDistance = (function() {
+    var body = document.body;
+    var bodyHeight = $(body).height();
+    // console.log('bodyHeight = ' + bodyHeight );
+    var underlayHeight = $(underlay).height();
+    // console.log('underlayHeight = ' + underlayHeight );
+    if (bodyHeight > underlayHeight) {
+      return ( bodyHeight + 100 ) ;
+    } else {
+      return ( bodyHeight + 100 );
+    }
+  }() );
+
+  // console.log("min vertical distance for balloons to float = " + minYDistance);
   //================================================
   //  Velocity Creating Effects
   //    - Requires Velocity.ui.js
@@ -76,17 +90,18 @@ $( window ).load(function() {
     { elements: overlay , properties: { opacity: [ 0 , 1 ] }, options: { duration: 1500, display: "none"} },    //* CSS */
     { elements: underlay , properties: "fadeIn", options: { duration: 2000 ,sequenceQueue: false} },           //* CSS */
     { elements: cakeObj , properties: { scale: [ 0.5 , 1 ] }, options: { duration: 1500 } },           //* CSS */
-    { elements: cakeObj , properties: "reverse", options: { duration: 1500 } },                        //* CSS */                
+    { elements: bannerObj, properties: { translateY: [ "0px" , -minYDistance ] }, options: { duration: 4000, sequenceQueue: false } },   
+    { elements: cakeObj , properties: "reverse", options: { duration: 1500, sequenceQueue: false } },                        //* CSS */             
     { elements: flames , properties: { translateX: [ 0.8 , 0 ], translateY: [ 4.8 , 0 ] , scale: [ 0.8 , 1 ] }, options: { duration: 1000, loop: true } },
-    { elements: blowOutButton, properties: "fadeIn", options: { duration: 500, display: "block" , delay: 1000, sequenceQueue: false } },  //* CSS */
+    // { elements: bannerObj, properties: { translateY: [ "0px" , -minYDistance ] }, options: { duration: 2000, sequenceQueue: false } },   
+    { elements: blowOutButton, properties: "fadeIn", options: { duration: 500, display: "block", delay: 1000, sequenceQueue: false } },  //* CSS */
     { elements: blowOutButton, properties: "callout.shake" }                                                                              //* CSS */
   ];
   var blowOutSequence = [
     { elements: blowOutButton, properties: "fadeOut", options: { duration: 500, display: "none" } },            //* CSS */
     { elements: flames, properties: "flameSmother" },
     { elements: smokes, properties: "smokeStroke" },
-    { elements: banner, properties: { translateY: [ "0px" , "-125px" ] }, options: { duration: 2000 } },
-    { elements: bRow, properties: { translateY: bMinDistance }, options: { duration: 5000, sequenceQueue: false } },
+    { elements: balloonsObj, properties: { translateY: ["0px", minYDistance ] }, options: { duration: 5000, sequenceQueue: false } },
     { elements: replayButton, properties: "fadeIn", options: { duration: 500, display: "block" } } //* CSS */ //show replay button at end of animation
   ];
   // var cakeContainerSequence = [
@@ -124,16 +139,18 @@ $( window ).load(function() {
   //    - (http://www.kirupa.com/html5/handling_events_for_many_elements.htm)
   //================================================ 
   function initialAnimation(event) {
-    var clickedItem = event.target.id;                  
-    if (clickedItem === 'continue' || 'close') {        //when #continue or #close are clicked
+    // var clickedItem = event.target.id;                  
+    // if (clickedItem === 'continue' || 'close') {        //when #continue or #close are clicked
+    if (event.target.id === 'continue' || 'close') {        //when #continue or #close are clicked
       // console.log('event targeted by ' + clickedItem);
-      $(banner).velocity({ translateY: [ "-125px" , "0px" ] });
+      $(bannerObj).velocity( { translateY: [ -minYDistance , "0px" ] } ); //move out of view.  animated back to place in cakeContainerSequence
+      $(balloonsObj).velocity( {translateY: [ minYDistance , "0px" ] } ); //move out of view.  animated back to place in blowOutSequence
       $.Velocity.RunSequence(cakeContainerSequence);    //run cakeContainerSequence
     }
     event.stopPropagation();                            //event to stop traversing the DOM under all situations once it gets overheard
   }
   function continuedAnimation(event) {
-    var clickedItem = event.target.id;
+    // var clickedItem = event.target.id;
     if (event.target.id === 'blow_out' ) {              //when #blow_out is clicked
       // console.log('event targeted by ' + clickedItem);
       $(flames)
