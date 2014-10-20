05e122b4e09664bc0736185794af699a74d5e3bc
kfehlberg <fehlbergkatie@gmail.com>
2014-10-20 16:27:53 -0500

    e.target.id || e.srcElement.id

--- js/bday_animate_with_fallbacks.js
+++ js/bday_animate_with_fallbacks.js
@@ -113,31 +113,92 @@ $( window ).load(function() {
     }
   }
   //================================================
+  //  FUNCTION EVENTSlisteners  Utilities functions
+  //================================================ 
+  // function addListeners( element, functionName ) {
+  //   if (element.addEventListener) {                      //if addEventListener is supported
+  //     element.addEventListener( 'click' , functionName, false );
+  //   } else {                                             //else IE fallback
+  //     element.attachEvent( 'onclick' , functionName );     
+  //     console.log('Event listener added via IE attachEvent fallback');  
+  //   }
+  // }
+  function getTarget(e) {
+    if (!e) {
+      e = window.event;
+    }
+    console.log('event target = ' + e.target.id + ' or for IE5-8 = ' + e.srcElement.id);
+    return e.target || e.srcElement;                //e.target  is the target of the event e.srcElement is IE5-8 equivalent
+  }
+  function stopEvent(e) {
+    if (!e) {                         //IE5-8
+      var e = window.event;           //IE5-8 eventObject is accessed via window
+      e.cancelBubble = true;          //IE5-8 equivalent to stopPropagation()
+      e.returnValue = false;          //IE5-8 equivalent to preventDefault()
+    }
+    if (e.stopPropagation) {
+      e.stopPropagation();            //Stops event bubbling or capturing further
+    }
+    if (e.preventDefault) {
+      e.preventDefault();             //cancel default behavior of event
+    }
+    return false;                     //if none of these options work- return: false prevents default behavior and further bubbling
+                                      //works in all browsers BUT will stop interpreter from processing any subsequent code within function
+  }
+    //http://ejohn.org/projects/flexible-javascript-events/#postcomment
+  function addEvent(element, eventType, callbackFunc) {
+    if (element.addEventListener) {  
+      element.addEventListener(eventType, callbackFunc, false); //if addEventListeners works, use it
+    } else {
+      element['e' + eventType + callbackFunc] = callbackFunc;       //IE5-8 callback method of el 
+      element[eventType + callbackFunc] = function() {          //Add second method
+        element['e' + eventType + callbackFunc](window.event);    //use it to call previous function
+      };
+      element.attachEvent('on' + eventType, element[eventType + callbackFunc]); //use attachEvent to call second function which then calls the first
+    }
+  }
+  // //http://ejohn.org/projects/flexible-javascript-events/#postcomment
+  // function removeEvent(element, eventType, callbackFunc) {
+  //   if (element.removeEventListener) {
+  //     element.removeEventListener(eventType, callbackFunc, false);
+  //   } else {
+  //     element.detachEvent( 'on' + eventType, element[eventType + callbackFunc]);
+  //     element[eventType + callbackFunc] = null;
+  //   }
+  // }
+   //================================================
   //  FUNCTION DEF Event Listener Fallback 
   //================================================ 
-  function eventsFallback( element, functionName ) {
-    if (element.addEventListener) {                      //if addEventListener is supported
-      element.addEventListener( 'click' , functionName, false );
-    } else {                                             //else IE fallback
-      element.attachEvent( 'onclick' , functionName );     
-      console.log('Event listener added via IE attachEvent fallback');  
-    }
-  }
+  // function initialAnimation(e) {
+  //   var targetObj = getTarget(e);
+  //   if (targetObj.id === 'contiue' || 'close') {
+  //     determineOpeningAnimation();
+  //   }
+  //   // if (e.target.id === 'continue' || 'close') {    //when #continue or #close are clicked
+  //     // determineOpeningAnimation();
+  //   // } 
+  //   stopEvent(event);
+  //   // event.stopPropagation(); 
+  //                            //event to stop traversing the DOM under all situations once it gets overheard
+  // }
+  // function continuedAnimation(e) {
+  //   var targetObj = getTarget(e);
+  //   if (targetObj.id === 'blow_out' ) {              //when #blow_out is clicked
+  //     scFinishingAnimation();
+  //   } else if (targetObj.id === 'replay') {          //when #replay is clicked
+  //     window.location.reload();                         //reload the page
+  //   } 
+  //   // if (e.target.id === 'blow_out' ) {              //when #blow_out is clicked
+  //   //   scFinishingAnimation();
+  //   // } else if (e.target.id === 'replay') {          //when #replay is clicked
+  //   //   window.location.reload();                         //reload the page
+  //   // } 
+  //   // event.stopPropagation();                            //event to stop traversing the DOM under all situations once it gets overheard
+  //   stopEvent(e);
+  // }
+
   //http://blog.patricktresp.de/2012/02/internet-explorer-8-and-all-the-fun-stuff-e-stoppropagation-e-preventdefault-mousedown/
-  function stopEvent( evt ) {
-    if(!evt) {
-      var evt = window.event;
-      evt.cancelBubble = true;
-      evt.returnValue = false;
-    }
-    if (evt.stopPropagation) { 
-      evt.stopPropagation(); 
-    }
-    if (evt.preventDefault) {
-      evt.preventDefault();
-    }
-    return false;
-  }
+  
   //================================================ 
   // FUNCTION DEF ANIMATION 
   //================================================ 
@@ -199,28 +260,39 @@ $( window ).load(function() {
   //    - event listeners are added to a parent of the buttons that are clicked 
   //    - (http://www.kirupa.com/html5/handling_events_for_many_elements.htm)
   //================================================ 
-  function initialAnimation(event) {
-    // if (addEventListener)
-    if (event.target.id === 'continue' || 'close') {    //when #continue or #close are clicked
+  function initialAnimation(e) {
+    var targetObj = getTarget(e);
+    if (targetObj.id === 'contiue' || 'close') {
       determineOpeningAnimation();
-    } 
+    }
+    // if (e.target.id === 'continue' || 'close') {    //when #continue or #close are clicked
+      // determineOpeningAnimation();
+    // } 
     stopEvent(event);
     // event.stopPropagation(); 
                              //event to stop traversing the DOM under all situations once it gets overheard
   }
-  function continuedAnimation(event) {
-    if (event.target.id === 'blow_out' ) {              //when #blow_out is clicked
+  function continuedAnimation(e) {
+    var targetObj = getTarget(e);
+    if (targetObj.id === 'blow_out' ) {              //when #blow_out is clicked
       scFinishingAnimation();
-    } else if (event.target.id === 'replay') {          //when #replay is clicked
+    } else if (targetObj.id === 'replay') {          //when #replay is clicked
       window.location.reload();                         //reload the page
     } 
+    // if (e.target.id === 'blow_out' ) {              //when #blow_out is clicked
+    //   scFinishingAnimation();
+    // } else if (e.target.id === 'replay') {          //when #replay is clicked
+    //   window.location.reload();                         //reload the page
+    // } 
     // event.stopPropagation();                            //event to stop traversing the DOM under all situations once it gets overheard
-    stopEvent(event);
+    stopEvent(e);
   }
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   //        FUNCTIONS CALLED
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   imageFallback();
-  eventsFallback(overlay, initialAnimation);
-  eventsFallback(underlay, continuedAnimation);
+  // addListeners(overlay, initialAnimation);
+  // addListeners(underlay, continuedAnimation);
+  addEvent(overlay, 'click', initialAnimation);
+  addEvent(underlay, 'click', continuedAnimation);
 });
