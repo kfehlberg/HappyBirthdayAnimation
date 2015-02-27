0e19062a9c881d187759a7e171fe5c9041ebd078
kfehlberg <fehlbergkatie@gmail.com>
2015-02-26 18:40:30 -0600

    update to stopProp
    p a links and button to github

--- js/bday_animate_with_fallbacks.js
+++ js/bday_animate_with_fallbacks.js
@@ -18,6 +18,7 @@ $( window ).load(function() {
   var cakeObj = document.getElementById("cake_obj");
   var bannerObj = document.getElementById("banner_obj"); 
   var balloonsObj = document.getElementById("balloons_obj");
+  
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   //        ANIMATION VARIABLES DEFINED
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -28,6 +29,9 @@ $( window ).load(function() {
     var underlay = document.getElementById("underlay");
     var blowOutButton = document.getElementById("blow_out");
     var replayButton = document.getElementById("replay");
+    var githubButton = document.getElementById("github");
+    
+
     //---------------------------------------
     var minYDistance = (function() {
       var body = document.body;
@@ -38,14 +42,15 @@ $( window ).load(function() {
     var openingSequence = [
       { elements: overlay , properties: { opacity: [ 0 , 1 ] }, options: { duration: 1500, delay: 100, display: "none"} },
       { elements: cakeObj , properties: { scale: [ 0.5 , 1 ] }, options: { duration: 1500 } },
-      { elements: bannerObj, properties: { translateY: [ "0px" , -minYDistance ] }, options: { duration: 4000, sequenceQueue: false } },   
+      { elements: bannerObj, properties: { translateY: [ "0px" , -minYDistance ] }, options: { duration: 4000, display: "block", sequenceQueue: false } },   
       { elements: cakeObj , properties: "reverse", options: { duration: 1500, sequenceQueue: false } },
       { elements: blowOutButton, properties: "fadeIn", options: { duration: 500, display: "block" } },
       { elements: blowOutButton, properties: "callout.shake" }  
     ];
     var finishingSequence = [
-      { elements: balloonsObj, properties: { translateY: [ "0px", minYDistance ] }, options: { duration: 5000 } },
-      { elements: replayButton, properties: "fadeIn", options: { duration: 500, display: "block" } } //show replay button at end of animation
+      { elements: balloonsObj, properties: { translateY: [ "0px", minYDistance ] }, options: { duration: 5000, display:"block" } },
+      { elements: replayButton, properties: "fadeIn", options: { duration: 500, display: "inline-block" } }, //show replay button at end of animation
+      { elements: githubButton, properties: "fadeIn", options: { duration: 500, display: "inline-block", sequenceQueue: false } } //show github button at end of animation
     ];
   //================================================ 
   // SMIL ANIMATION VARIABLES
@@ -94,7 +99,6 @@ $( window ).load(function() {
     }
   }
   function imageFallback() {
-    // if (!Modernizr.smil) {  //does not support SMIL
     if (noSmil && true ) {  //does not support SMIL
       if (Modernizr.svg) {  //but does support SVG
         objSvgToPng(cakeObj);
@@ -108,52 +112,54 @@ $( window ).load(function() {
         }
         // console.log('Boo. SMIL and SVG are not supported');
       }
-    } else {
-      // console.log('YAY! SMIL animation supported!');
     }
+    // else {
+    //   // console.log('YAY! SMIL animation supported!');
+    // }
   }
   //================================================
   //  FUNCTION EVENTSlisteners  Utilities functions
   //================================================ 
-  // function addListeners( element, functionName ) {
-  //   if (element.addEventListener) {                      //if addEventListener is supported
-  //     element.addEventListener( 'click' , functionName, false );
-  //   } else {                                             //else IE fallback
-  //     element.attachEvent( 'onclick' , functionName );     
-  //     console.log('Event listener added via IE attachEvent fallback');  
-  //   }
-  // }
   function getTarget(e) {
     if (!e) {
       e = window.event;
       // console.log('event target for IE5-8 = ' + e.srcElement.id);
     }
     // console.log('event target = ' + e.target.id + ' or for IE5-8 = ' + e.srcElement.id);
-    return e.target || e.srcElement;                //e.target  is the target of the event e.srcElement is IE5-8 equivalent
+    return e.target || e.srcElement;  //e.target  is the target of the event e.srcElement is IE5-8 equivalent
+  }
+  function stopPropagationOnly(e) {
+    if (!e) {                         //IE5-8
+      e.cancelBubble = true;          //IE5-8 equivalent to stopPropagation()
+    } else if (e.stopPropagation) {
+      e.stopPropagation();            //Stops event bubbling or capturing further
+    } else {
+      return false;
+    }
   }
   function stopEvent(e) {
-    if (!e) {                         //IE5-8
-      var e = window.event;           //IE5-8 eventObject is accessed via window
-      e.cancelBubble = true;          //IE5-8 equivalent to stopPropagation()
-      e.returnValue = false;          //IE5-8 equivalent to preventDefault()
+    if (!e) {                         //  * IE5-8
+      e.cancelBubble = true;          //    IE5-8 equivalent to stopPropagation()
+      e.returnValue = false;          //    IE5-8 equivalent to preventDefault()
     }
-    if (e.stopPropagation) {
-      e.stopPropagation();            //Stops event bubbling or capturing further
+    if (e.stopPropagation) {          //  * Browser supports .stopProp
+      e.stopPropagation();            //    Stops event bubbling or capturing further
     }
-    if (e.preventDefault) {
-      e.preventDefault();             //cancel default behavior of event
-    }
-    return false;                     //if none of these options work- return: false prevents default behavior and further bubbling
+    if (e.preventDefault) {           //  * Browser supports .preventDef
+      e.preventDefault();             //    cancel default behavior of event
+    } else {
+      return false;                   //if none of these options work- return: false prevents default behavior and further bubbling
                                       //works in all browsers BUT will stop interpreter from processing any subsequent code within function
+    } 
   }
     //http://ejohn.org/projects/flexible-javascript-events/#postcomment
   function addEvent(element, eventType, callbackFunc) {
-    if (element.addEventListener) {  
+    if (element.addEventListener) { 
       element.addEventListener(eventType, callbackFunc, false); //if addEventListeners works, use it
     } else {
-      element['e' + eventType + callbackFunc] = callbackFunc;       //IE5-8 callback method of el 
+      element['e' + eventType + callbackFunc] = callbackFunc;   //IE5-8 callback method of el 
       element[eventType + callbackFunc] = function() {          //Add second method
-        element['e' + eventType + callbackFunc](window.event);    //use it to call previous function
+        element['e' + eventType + callbackFunc](window.event);  //use it to call previous function
       };
       element.attachEvent('on' + eventType, element[eventType + callbackFunc]); //use attachEvent to call second function which then calls the first
     }
@@ -167,37 +173,6 @@ $( window ).load(function() {
   //     element[eventType + callbackFunc] = null;
   //   }
   // }
-   //================================================
-  //  FUNCTION DEF Event Listener Fallback 
-  //================================================ 
-  // function initialAnimation(e) {
-  //   var targetObj = getTarget(e);
-  //   if (targetObj.id === 'contiue' || 'close') {
-  //     determineOpeningAnimation();
-  //   }
-  //   // if (e.target.id === 'continue' || 'close') {    //when #continue or #close are clicked
-  //     // determineOpeningAnimation();
-  //   // } 
-  //   stopEvent(event);
-  //   // event.stopPropagation(); 
-  //                            //event to stop traversing the DOM under all situations once it gets overheard
-  // }
-  // function continuedAnimation(e) {
-  //   var targetObj = getTarget(e);
-  //   if (targetObj.id === 'blow_out' ) {              //when #blow_out is clicked
-  //     scFinishingAnimation();
-  //   } else if (targetObj.id === 'replay') {          //when #replay is clicked
-  //     window.location.reload();                         //reload the page
-  //   } 
-  //   // if (e.target.id === 'blow_out' ) {              //when #blow_out is clicked
-  //   //   scFinishingAnimation();
-  //   // } else if (e.target.id === 'replay') {          //when #replay is clicked
-  //   //   window.location.reload();                         //reload the page
-  //   // } 
-  //   // event.stopPropagation();                            //event to stop traversing the DOM under all situations once it gets overheard
-  //   stopEvent(e);
-  // }
-
   //http://blog.patricktresp.de/2012/02/internet-explorer-8-and-all-the-fun-stuff-e-stoppropagation-e-preventdefault-mousedown/
   
   //================================================ 
@@ -216,7 +191,7 @@ $( window ).load(function() {
       reset: {'backgroundPositionX': '-3000px' }
     });
   //------------------------------------------------
-  function scOpeningAnimation() {
+  function scOpeningAnimation() { //used in browsers that support SMIL animation(smilOpeningAnimation) AND in browsers that do not
     $.Velocity( bannerObj, { translateY: [ -minYDistance , "0px" ] } ); //move out of view.  animated back to place in cakeContainerSequence
     $.Velocity( balloonsObj, { translateY: [ minYDistance , "0px" ] } ); //move out of view.  animated back to place in blowOutSequence
     $.Velocity.RunSequence(openingSequence);
@@ -226,11 +201,8 @@ $( window ).load(function() {
     $(flames).velocity( { translateX: [ 0.8 , 0 ], translateY: [ 4.8 , 0 ] , scale: [ 0.8 , 1 ]  }, { duration:1000, loop: true });
   }
   function determineOpeningAnimation() {
-    // if (Modernizr.smil) {
-    if (yesSmil && true) {
-    // if (cakeObj.className === 'svg') { //if the svg has not been replaced by png (imageFallback determined svg and smil were supported)
+    if (yesSmil && true) { //if smil is supported
       smilOpeningAnimation();
-    // } else if (Modernizr.cssanimations) {
     } else  {
       scOpeningAnimation();
     } 
@@ -245,16 +217,15 @@ $( window ).load(function() {
   //------------------------------------------------
   function scFinishingAnimation() {
     $(blowOutButton).velocity( "fadeOut" , { duration: 500, display: "none" } );
-    // if (cakeObj.className === 'svg') { //if the svg has not been replaced by png (imageFallback determined svg and smil were supported)
-    if (yesSmil && true) {
-    // if (Modernizr.smil) {
-      $(flames).velocity( "stop");        //stop running looped animation on flames
+    if (yesSmil && true) {                                      //if smil animation is supported
+      $(flames).velocity( "stop");                              //stop running looped animation on flames
+      //sequence of flames blown out and smoke rising
       $(flames).velocity({ translateX: [ 4 , 0 ] , translateY: [ 24 , 0 ]  , scale: [ 0 , 1 ] }, 1000, function() {
         $(smokes).velocity( { strokeDashoffset: [ "0", "110"] }, 1500 ).velocity( { strokeDashoffset: [ "-110", "0"] }, 1500 );
       });
       $.Velocity.RunSequence(finishingSequence);
-    } else {
-      $(cakeObj).velocity( "spriteSmotherNSmoke" );
+    } else {                                                    //if SMIL animation is not supported,
+      $(cakeObj).velocity( "spriteSmotherNSmoke" );             //use sprite animation for blowing out candles
       $.Velocity.RunSequence(finishingSequence);
     }
   }
@@ -265,37 +236,33 @@ $( window ).load(function() {
   //================================================ 
   function initialAnimation(e) {
     var targetObj = getTarget(e);
-    if (targetObj.id === 'contiue' || 'close') {
-      determineOpeningAnimation();
+    if (targetObj.id === 'continue') {                        //  * when #continue is clicked
+      determineOpeningAnimation();                            //    - figure out which animation to use/use it
+      stopEvent(e);                                           //    - event to stop traversing the DOM under all situations once it gets overheard
+    } else if (targetObj.id === 'modernizr' || 'velocity') {  //  * when user clicks on #modernizr or #velocity links
+      stopPropagationOnly(e);                                 //    - stops Propagation so that user may follow link
+    } else {                                                  //  * other cases
+      stopEvent(e);                                           //    - event to stop traversing the DOM under all situations once it gets overheard
     }
-    // if (e.target.id === 'continue' || 'close') {    //when #continue or #close are clicked
-      // determineOpeningAnimation();
-    // } 
-    stopEvent(e);
-    // event.stopPropagation(); 
-                             //event to stop traversing the DOM under all situations once it gets overheard
   }
   function continuedAnimation(e) {
     var targetObj = getTarget(e);
-    if (targetObj.id === 'blow_out' ) {              //when #blow_out is clicked
-      scFinishingAnimation();
-    } else if (targetObj.id === 'replay') {          //when #replay is clicked
+    if (targetObj.id === 'blow_out' ) {              // * when #blow_out is clicked
+      scFinishingAnimation();                        //   - call to determine which animation to use/use it
+      stopEvent(e);                                  //   - event to stop traversing the DOM under all situations once it gets overheard
+    } else if (targetObj.id === 'replay') {          // * when #replay is clicked
       window.location.reload();                         //reload the page
+      stopEvent(e);   //event to stop traversing the DOM under all situations once it gets overheard
+    } else if (targetObj.id === 'github') {
+      stopPropagationOnly(e); //stops Propagation so that user may follow link
+    } else {
+      stopEvent(e);   //event to stop traversing the DOM under all situations once it gets overheard
     } 
-    // if (e.target.id === 'blow_out' ) {              //when #blow_out is clicked
-    //   scFinishingAnimation();
-    // } else if (e.target.id === 'replay') {          //when #replay is clicked
-    //   window.location.reload();                         //reload the page
-    // } 
-    // event.stopPropagation();                            //event to stop traversing the DOM under all situations once it gets overheard
-    stopEvent(e);
   }
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   //        FUNCTIONS CALLED
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   imageFallback();
-  // addListeners(overlay, initialAnimation);
-  // addListeners(underlay, continuedAnimation);
   addEvent(overlay, 'click', initialAnimation);
   addEvent(underlay, 'click', continuedAnimation);
 });
